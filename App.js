067474271953b47ec51aeeadf69983c2ccafe9f3
Hawaijar.js <hawaijar.js@gmail.com>
2017-11-25 12:19:09 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textStyle}>Open up App.js to start working on your app!!!</Text>
+        <Text style={styles.textStyle}>Changes you make will automatically reload.</Text>
+        <Text style={styles.textStyle}>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rebeccapurple',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: 'white'
+  }
+});
