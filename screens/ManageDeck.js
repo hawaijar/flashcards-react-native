e7216f8cb28f7b3a5fd589ea25e3aa8a638ba7d9
Hawaijar.js <hawaijar.js@gmail.com>
2017-11-26 20:45:52 +0530

    Added functionality to add Decks to AsyncStorage

--- screens/ManageDeck.js
+++ screens/ManageDeck.js
@@ -1,13 +1,84 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  AsyncStorage,
+  Alert
+} from 'react-native';
 
 class ManageDeck extends Component {
+  constructor() {
+    super();
+    this._isStoreExist = this._isStoreExist.bind(this);
+    this._createStore = this._createStore.bind(this);
+    this._addDeck = this._addDeck.bind(this);
+  }
   state = {
     title: ''
   };
+  static KEY_STORAGE = 'decks';
   handleChange = title => {
     this.setState({ title });
   };
+
+  async _isStoreExist() {
+    const keyValue = await AsyncStorage.getItem(ManageDeck.KEY_STORAGE);
+    return keyValue;
+  }
+  async _createStore() {
+    const store = {};
+    try {
+      await AsyncStorage.setItem(ManageDeck.KEY_STORAGE, JSON.stringify(store));
+      return JSON.stringify(store);
+    } catch (e) {
+      throw new Error(`Error creating a store: ${e}`);
+    }
+  }
+  async _addDeck(store) {
+    const DECK = this.state.title
+      .trim()
+      .toLowerCase()
+      .replace(/\b\w/g, l => l.toUpperCase());
+    if (Object.keys(store).includes(DECK)) {
+      //Alert.alert(`Deck ${DECK} successfully added!`);
+      Alert.alert(`Deck ${DECK} is already existing!`);
+      return store;
+    } else {
+      store[`${DECK}`] = {
+        title: `${DECK}`,
+        questions: []
+      };
+      const serializedStore = JSON.stringify(store);
+      await AsyncStorage.setItem(
+        `${ManageDeck.KEY_STORAGE}`,
+        serializedStore,
+        () => {
+          Alert.alert(`Deck ${DECK} successfully added!`);
+        }
+      );
+      return store;
+    }
+  }
+
+  handleSubmit = () => {
+    if (this.state.title !== '') {
+      this._isStoreExist()
+        .then(store => {
+          if (store === null) {
+            this._createStore().then(store => console.log(store));
+          } else {
+            this._addDeck(JSON.parse(store)).then(store => {
+              console.log(store);
+              this.setState({ title: '' });
+            });
+          }
+        })
+        .catch(e => console.log(e));
+    }
+  };
+
   render() {
     return (
       <View style={Styles.container}>
@@ -21,7 +92,10 @@ class ManageDeck extends Component {
           onChangeText={this.handleChange}
           value={this.state.title}
         />
-        <TouchableOpacity style={Styles.submitButton}>
+        <TouchableOpacity
+          onPress={this.handleSubmit}
+          style={Styles.submitButton}
+        >
           <Text style={Styles.submitButtonText}> Submit </Text>
         </TouchableOpacity>
       </View>
