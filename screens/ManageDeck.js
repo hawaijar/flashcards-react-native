de842865ba7ca3707def1f2725251050af71298d
Hawaijar.js <hawaijar.js@gmail.com>
2017-11-26 06:40:20 +0530

    Added UI screen of Manage Deck

--- screens/ManageDeck.js
+++ screens/ManageDeck.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-const ManageDeck = () => {
-  return (
-    <View style={Styles.container}>
-      <Text style={Styles.textStyle}>ManageDeck screen</Text>
-    </View>
-  )
+class ManageDeck extends Component {
+  state = {
+    title: ''
+  };
+  handleChange = title => {
+    this.setState({ title });
+  };
+  render() {
+    return (
+      <View style={Styles.container}>
+        <Text style={Styles.textStyle}>
+          What is the title of your new deck?
+        </Text>
+        <TextInput
+          placeholder="Deck Title"
+          placeholderTextColor="#9a73ef"
+          style={Styles.textInputStyle}
+          onChangeText={this.handleChange}
+          value={this.state.title}
+        />
+        <TouchableOpacity style={Styles.submitButton}>
+          <Text style={Styles.submitButtonText}> Submit </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 export default ManageDeck;
@@ -14,10 +34,32 @@ export default ManageDeck;
 const Styles = {
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center'
   },
   textStyle: {
-    fontSize: 30
+    fontSize: 40,
+    padding: 5
+  },
+  textInputStyle: {
+    marginTop: 30,
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 5,
+    alignSelf: 'stretch',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5
+  },
+  submitButton: {
+    backgroundColor: '#7a42f4',
+    padding: 10,
+    margin: 15,
+    height: 40
+  },
+  submitButtonText: {
+    color: 'white'
   }
-}
+};
